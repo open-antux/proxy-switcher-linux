4a991d9510db16dba9c8a170c5df7e83f1e730aa
Antonio Giustino <antonio.giustino11@gmail.com>
2019-07-26 08:35:11 +0200

    Updated file

--- proxy-switcher-linux.py
+++ proxy-switcher-linux.py
@@ -1,4 +1,13 @@
 import os
+import urllib.request
 #from bs4 import BeautifulSoup as bs
 
-os.system("wget https://www.proxynova.com/proxy-server-list/")
+def download_index(site):
+    try:
+        urllib.request.urlretrieve(site, "./proxy-list.html")
+    except urllib.error.URLError as e:
+        print("\nThere was an error: " + str(e))
+
+
+if __name__ == '__main__':
+    download_index("https://www.proxynova.com/proxy-server-list/")
