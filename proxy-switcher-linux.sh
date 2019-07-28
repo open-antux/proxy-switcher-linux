a9461d968b3ada030a57d13b88daa258de08ceea
Antonio Giustino <antonio.giustino11@gmail.com>
2019-07-28 17:50:57 +0200

    Updated file

---
+++ proxy-switcher-linux.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function start {
+	export {http,https,ftp,rsync}_proxy="http://$ipaddr:$port"
+	export no_proxy="localhost, 127.0.0.1"
+}
+
+
+if [[ $1 != "" ]]
+then
+	stop #Stop all proxy
+	ipaddr=$1
+	if [[ $2 == "" ]]
+	then
+		port="8080"
+	else
+		port=$2
+	fi
+	start #Start to setup proxy
+else
+	stop
+fi
