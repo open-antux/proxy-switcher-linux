a2f0407fc37f965dadce46d8e9bb5aea6a3ae11e
Antonio Giustino <antonio.giustino11@gmail.com>
2019-07-29 07:45:26 +0200

    Updated file

--- proxy-switcher-linux.sh
+++ proxy-switcher-linux.sh
@@ -3,6 +3,30 @@
 function start {
 	export {http,https,ftp,rsync}_proxy="http://$ipaddr:$port"
 	export no_proxy="localhost, 127.0.0.1"
+
+	#Setup proxy with socks
+	cat /etc/tsocks.conf &> /dev/null
+	if [[ $? != 1  ]]
+	then
+		echo "server = $ipaddr" > /etc/tsocks
+		echo "server_port = $port" >> /etc/tsocks
+	fi
+
+	#Setup proxy with gsettings
+	if [[ $XDG_CURRENT_DESKTOP == "GNOME" ]]
+	then
+		gsettings set org.gnome.system.proxy mode 'manual'
+
+		gsettings set org.gnome.system.proxy.http host "$ipaddr"
+		gsettings set org.gnome.system.proxy.https host "$ipaddr"
+		gsettings set org.gnome.system.proxy.ftp host "$ipaddr"
+		
+		gsettings set org.gnome.system.proxy.http port "$port"
+		gsettings set org.gnome.system.proxy.https port "$port"
+		gsettings set org.gnome.system.proxy.ftp port "$port"
+		
+		gsettings set org.gnome.system.proxy ignore-hosts "['localhost', '127.0.0.0/8', '10.0.0.0/8', '192.168.0.0/16', '172.16.0.0/12' , '*.localdomain.com' ]"
+	fi
 }
 
 
